2e2c00f133ce594777ab4370b13019f504d346e7
ALSRKAL <95968239+ALSRKAL@users.noreply.github.com>
2022-01-29 15:48:41 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: SingleChildScrollView(
+      body: ListView(
       	child: Container(
 	        child: Column(
 	          children: <Widget>[
